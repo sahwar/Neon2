ea1cac29224d3c9db074ad659cb5240891902dff
zoemcl <zoemmclennan@gmail.com>
2018-08-17 15:40:05 -0400

    Intersection is detected
    It is still not funcitonal yet, but the svg intersetion is detected

--- src/DragHandler.js
+++ src/DragHandler.js
@@ -44,27 +44,75 @@ function DragHandler (neonView) {
 
         function dragEnded () {
             dragEndCoords = [d3.event.x, d3.event.y];
+            var xdiff = dragEndCoords[0] - dragStartCoords[0];
+            var ydiff = dragEndCoords[1] - dragStartCoords[1];
             let paramArray = [];
+            var intersect = false;
+            var inEl = false;
+            
             selection.forEach((el) => {
                 let singleAction = { action: 'drag', param: { elementId: el.id,
-                    x: parseInt(dragEndCoords[0] - dragStartCoords[0]),
-                    y: parseInt(dragEndCoords[1] - dragStartCoords[1]) * -1}
+                    x: parseInt(xdiff),
+                    y: parseInt(ydiff * -1) }
                 };
                 paramArray.push(singleAction);
+
+                //check for overlap of nc elements upon drag
+                var bbox = el.getBBox();
+                var l = bbox.x + xdiff;
+                var r = bbox.x + bbox.width + xdiff;
+                var t = bbox.y + ydiff;
+                var b = bbox.y + bbox.height - (ydiff);
+    
+                var layerParent = $(el).closest(".layer");
+            
+                if(layerParent.length == 1){
+                    var ncChildren = Array.from($(layerParent).find(".nc"));
+                    ncChildren.forEach(nc => { 
+                        if($(el).hasClass("nc")){
+                            inEl = el.id == nc.id;
+                        }
+                        else{
+                            var elNcs = Array.from($(el).find(".nc"));
+                            elNcs.forEach(elNc => {
+                                inEl = elNc.id == nc.id;
+                            })
+                        }
+                        if(!inEl){
+                            var ncBBox = nc.getBBox();
+                            var ncL = ncBBox.x;
+                            var ncR = ncBBox.x + ncBBox.width;
+                            var ncT = ncBBox.y;
+                            var ncB = ncBBox.y + ncBBox.height;
+        
+                            var horizIntersect = l < ncR && r > ncL;
+                            var vertIntersect = t < ncB && b > ncT;
+        
+                            if(horizIntersect && vertIntersect) {
+                                console.log("intersect!")
+                                intersect=true;
+                            }
+                        }
+                    })
+                }
             });
             editorAction = {
                 "action": "chain",
                 "param": paramArray
             };
 
-            var xDiff = Math.abs(dragStartCoords[0] - dragEndCoords[0]);
-            var yDiff = Math.abs(dragStartCoords[1] - dragEndCoords[1]);
-
-            if(xDiff > 5 || yDiff > 5){
-                neonView.edit(editorAction);
+            if(Math.abs(xdiff) > 5 || Math.abs(ydiff) > 5){
+                if(!intersect){
+                    neonView.edit(editorAction);
+                }
                 neonView.refreshPage();
                 endOptionsSelection();
             }
+            else{
+                neonView.refreshPage();
+                endOptionsSelection();
+            }
+            
             dragInit();
         }
     }
@@ -72,6 +120,45 @@ function DragHandler (neonView) {
     function endOptionsSelection () {
         $("#moreEdit").empty();
         $("#moreEdit").addClass("is-invisible");
+    }
+
+    function checkIntersection(selection) {
+        var intersect = false;
+        selection.forEach(el => {
+            var bbox = el.getBBox();
+            var l = bbox.x;
+            var r = bbox.x + bbox.width;
+            var t = bbox.y;
+            var b = bbox.y + bbox.height;
+
+            //console.log(l + ", " + r + ", " + t + ", " + b);
+
+            var layerParent = $(el).closest(".layer");
+        
+            if(layerParent.length == 1){
+                var ncChildren = Array.from($(layerParent).find(".nc"));
+                
+                ncChildren.map(nc => {
+                    if(el.id != nc.id){
+                        var ncBBox = nc.getBBox();
+                        var ncL = ncBBox.x;
+                        var ncR = ncBBox.x + ncBBox.width;
+                        var ncT = ncBBox.y;
+                        var ncB = ncBBox.y + ncBBox.height;
+    
+                        //console.log(ncL + ", " + ncR + ", " + ncT + ", " + ncB);
+    
+                        var horizIntersect = l < ncR && r > ncL;
+                        var vertIntersect = t < ncB && b > ncT;
+    
+                        if(horizIntersect && vertIntersect) {
+                            console.log("intersect!");
+                        }
+                    }
+                })
+            }
+        
+        })
     }
 
     DragHandler.prototype.dragInit = dragInit;
